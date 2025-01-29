d9caf5de0d8a9ff59a63cf60d53984adb86a38b9
Keith Laspoña <laspona.keith@gmail.com>
2025-01-29 21:34:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,11 +5,8 @@ apt-get update
 
 # Install Python package dependencies
 pip install streamlit langchain
-pip install transformers
-pip install datasets
 
-# You might need to add additional setup commands here
-# For example, if you need to install other system packages or tools
+streamlit run app.py
 
 # Setup complete message
 echo "Setup complete!"
